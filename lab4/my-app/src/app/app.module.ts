e55b73ab77eae27937fb6e28ebdd10855252bdd4
Ergos1 <33097938+Ergos1@users.noreply.github.com>
2021-02-21 21:37:19 +0600

    updatelab4

--- lab4/my-app/src/app/app.module.ts
+++ lab4/my-app/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 
 import { FormsModule }   from '@angular/forms';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
+    TopBarComponent,
   ],
   imports: [
     BrowserModule,
